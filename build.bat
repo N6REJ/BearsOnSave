36b5c9797011d6255cad92855f1b79f121668d24
Troy T. Hall <programming@hallhome.us>
2020-03-10 05:26:21 -0500

    added build.bat better explanation in params.php

---
+++ build.bat
@@ -0,0 +1,29 @@
+@echo off
+echo "Starting build..."
+rd /s /q .build
+mkdir .build
+copy *.* .build
+del ".build\.*"
+del ".build\build.bat"
+xcopy /S /Y /I language .build\language
+xcopy /S /Y /I images .build\images
+xcopy /S /Y /I vendor .build\vendor
+xcopy /S /Y /I test .build\test
+
+echo "Creating zip file..."
+cd .build
+setlocal EnableDelayedExpansion
+FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
+    if "%%B" NEQ "" (
+        SET /A FDATE=%%F*10000+%%D*100+%%A
+        SET /A FTIME=%%B*10000+%%C*100+%%E
+    )
+)
+
+SET DatePartYear=%FDATE:~0,4%
+SET DatePartMonth=%FDATE:~4,2%
+SET DatePartDay=%FDATE:~6,2%
+call "C:\Program Files\7-Zip\7z.exe" a -y -r ..\.releases\bearsonsave-J3.9.%DatePartYear%.%DatePartMonth%.%DatePartDay%.zip *.*
+cd ..
+rd /s /q .build
+echo "Done..."
--- test/params.php
+++ test/params.php
@@ -1,8 +1,25 @@
 <?php
 defined('_JEXEC') or die;
 
-/* ====== DO NOT TOUCH ABOVE THIS LINE!! ===== */
+use Joomla\CMS\Filesystem\Path;
+use Joomla\CMS\Filesystem\File;
+use Joomla\CMS\Factory;
+
+// $data contains template params use $data->get('') to retrieve the params.
+// $css is the default variable required to be returned.
+// use the following to automatically output varname in the line above the css
+// if($debug){ $style .= "\n" . '/* $myvargoeshere */' . "\n";}
+
+if ( defined('JDEBUG') && JDEBUG )
+{
+	$debug = true;
+}
+else
+{
+	$debug = false;
+}
+
 $css = "#top{\n"
 	. "background-color: purple;\n"
 	. "color: yellow !important;\n"
-	."}\n";
+	. "}\n";
